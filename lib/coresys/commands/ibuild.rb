4ccef85564957c3f2773285559c2c4c31b20d6bd
Chris Hoffman <cehoffman@gmail.com>
2013-02-16 03:54:47 -0600

    Print directory incase Dir.cwd isn't honored on new shell

--- lib/coresys/commands/ibuild.rb
+++ lib/coresys/commands/ibuild.rb
@@ -1,6 +1,8 @@
-formula = Coresys::Formula.find(ARGV[0])
+formula = Coresys::Formula.find(ARGV[0]).new
 Coresys::Installer.new(formula).install do
+  Dir.chdir(Dir.pwd)
+  puts Dir.pwd
   system(ENV['SHELL'], '-l')
-  raise StandardError, "Interactive build canceled" unless $/.success?
+  raise StandardError, "Interactive build canceled" unless $?.success?
 end
 Coresys.link(ARGV[0])
