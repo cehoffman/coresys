ebd81a42282a263b99a95c98717f96553eb67520
Chris Hoffman <cehoffman@gmail.com>
2013-02-16 03:53:16 -0600

    Extend ENV correctly

--- lib/core_ext/env.rb
+++ lib/core_ext/env.rb
@@ -1,9 +1,17 @@
-module ENV
+module ENVHelpers
   ['CC', 'CXX', 'CFLAGS', 'CXFLAGS', 'LDFLAGS'].each do |var|
     class_eval <<-RUBY,__FILE__,__LINE__ + 1
       def #{var.downcase}
         self[#{var.inspect}]
       end
+
+      def #{var.downcase}=(val)
+        self[#{var.inspect}] = val.to_s
+      end
     RUBY
   end
 end
+
+ENV.extend ENVHelpers
+
+ENV.cc ||= 'gcc'
