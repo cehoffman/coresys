09873bf2168289239394c98206827a0c1ebc02e8
Chris Hoffman <cehoffman@gmail.com>
2013-02-20 02:22:09 -0600

    Fix stripping non letter for create namer

--- lib/coresys/commands/create.rb
+++ lib/coresys/commands/create.rb
@@ -1,6 +1,6 @@
 url = ARGV[0]
 name = url.split('/')[-1][/([a-zA-Z\-_]+)/, 1].gsub(/[-_]([a-zA-Z])?/) { |cap| cap.upcase if cap }
-name.rstrip!('-_')
+name.sub!(/[-_]+$/, '')
 
 (Coresys.formula + "#{name.downcase}.rb").open('wb') do |f|
   f.write <<-EOF
